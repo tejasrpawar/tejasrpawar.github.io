935103513d66aed86a41b05619baf08a84b59e86
tejasrpawar <tejasrpawar0108@gmail.com>
2025-06-06 18:26:13 -0600

    test

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/tejasrpawar.github.io' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/tejasrpawar.github.io/' : '',
+  trailingSlash: true,
 };
 
 const withMDX = createMDX({
