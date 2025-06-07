e56011f0bf7f0a66c95a032ee026c37825546b65
tejasrpawar <tejasrpawar0108@gmail.com>
2025-06-06 18:36:56 -0600

    fixed export issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,18 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import nextPlugin from '@next/eslint-plugin-next';
-import prettierPlugin from 'eslint-plugin-prettier';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      '@next/next': nextPlugin,
-      'prettier': prettierPlugin,
-    },
-    rules: {
-      '@next/next/no-html-link-for-pages': 'off',
-      'prettier/prettier': 'error',
-    },
-  }
-);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ),
+];
--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/tejasrpawar.github.io' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/tejasrpawar.github.io/' : '',
   trailingSlash: true,
+  distDir: 'out',
 };
 
 const withMDX = createMDX({
